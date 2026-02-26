a4be1a9418bdf86e0dc27126f74c538fdac7c30a
49318744-M <aina.rovira@estudiants.urv.cat>
2026-02-26 10:27:02 +0100

    lab3 guardar a bd

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,13 +9,21 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(
+        private CarRepository $carRepository
+    ) {}
     #[Route('/hello/demo', name: 'app_hello_demo')]
-    public function helloDemo(CarRepository $carRepository): Response
+    public function helloDemo(): Response
     {
-        $allCars = $carRepository->findAll();
-
-        return $this->render('demo/hello_demo.html.twig', [
-            'cars' => $allCars,
-        ]);
+        $car2 = $this->carRepository->find(2);
+        $car2->setName('Super Toyota');
+        $this->carRepository->save($car2, true);
+        $allCars = $this->carRepository->findCarsWithIdGreater(1);
+        return $this->render(
+            'demo/hello_demo.html.twig',
+            [ 'cars' => $allCars,
+            ]
+        );
     }
+
 }
--- src/Repository/CarRepository.php
+++ src/Repository/CarRepository.php
@@ -15,4 +15,18 @@ class CarRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Car::class);
     }
+    public function findCarsWithIdGreater(int $value): array
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->where($qb->expr()->gt('c.id', ':value'));
+        $qb->setParameter('value', $value);
+        return $qb->getQuery()->getResult();
+    }
+    public function save(Car $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 }
